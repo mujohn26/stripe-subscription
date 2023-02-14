98a7398236bfe40a91ffe3ec25c9e73c792277e2
MUGIRANEZA JOHN <52497006+mujohn26@users.noreply.github.com>
2023-02-14 09:47:21 +0200

    add 3d secure

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,28 +4,43 @@ class PaymentsController < ApplicationController
 
   def create
     @amount = 99
+  payment_method = Stripe::PaymentMethod.create({
+    type: 'card',
+    card: {
+      token: params[:stripeToken]
+    }
+  })
 
-    customer = Stripe::Customer.create({
-      email: params[:email],
-      source: params[:stripeToken],
-      name: params[:name]
-    })
 
-    charge = Stripe::Charge.create({
-      customer: customer.id,
-      amount: @amount,
-      description: 'Rails Stripe customer',
-      currency: 'usd',
-    })
+  payment_intent = Stripe::PaymentIntent.create({
+    amount: @amount,
+    currency: 'USD',
+    payment_method_types: ['card'],
+    payment_method: payment_method.id,
+    confirm: true,
+    use_stripe_sdk: true,
+    setup_future_usage: 'off_session'
+  })
 
-    user = User.new({name:params[:name],email:params[:email],stripe_token:params[:stripeToken]})
-    if user.save
-      cookies[:stripe_token] = params[:stripeToken]
-      redirect_to dashboard_view_path
-    else
-      redirect_to root_path
-    end
-    
+  if payment_intent.status == 'requires_action'
+      authentication_url = payment_intent.next_action.use_stripe_sdk.stripe_js
+      user = User.new({name:params[:name],email:params[:email],stripe_token:params[:stripeToken]})
+      if user.save
+        cookies[:stripe_token] = params[:stripeToken]
+        redirect_to authentication_url, allow_other_host: true
+      else
+        redirect_to root_path
+      end
+   else
+      user = User.new({name:params[:name],email:params[:email],stripe_token:params[:stripeToken]})
+      if user.save
+        cookies[:stripe_token] = params[:stripeToken]
+          redirect_to dashboard_view_path
+        else
+          redirect_to root_path
+      end
+  end
+
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to root_path
